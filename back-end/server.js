477839f5df1777d8acbeb43c9183975f474a1800
Kaillensopoulos <arnaud.scieur@gmail.com>
2018-10-19 09:58:53 +0200

    Server Filter Modif

--- back-end/server.js
+++ back-end/server.js
@@ -6,61 +6,55 @@ const nodemailer = require("nodemailer");
 
 const uri = "mongodb://Voyage:15oct18@ds223009.mlab.com:23009/arnaudscieur";
 
-const db = mongoose.connect( uri, function( err, response){
-    if(err){console.log(err);}
-    });
-    
-    var app = express();
-    app.use(bodyParser());
-    app.use(bodyParser.json({limit:'10mb'}));
-    app.use(bodyParser.urlencoded({extended: true}));
-    
-    app.use(function(req,res,next){
-      // res.setHeader( 'Access-Control-Allow-Origin', process.env.PORT);
-        res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
-        res.setHeader( 'Access-Control-Allow-Headers', 'Accept,Accept-Language,Content-Language,Content-Type');
-        res.setHeader( 'Access-Control-Expose-Headers', 'Content-Length,Content-Range');
+const db = mongoose.connect(uri, function (err, response) {
+    if (err) { console.log(err); }
+});
+
+var app = express();
+app.use(bodyParser());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+    // res.setHeader( 'Access-Control-Allow-Origin', process.env.PORT);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Accept,Accept-Language,Content-Language,Content-Type');
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length,Content-Range');
     next();
-    });
+});
 
-    app.get("/Filter", async function (req,res,){
-        let FilterList = req.query.Filter;
+app.get("/Filter", async function (req, res, ) {
+    let FilterList = req.query.Filter;
+    let Sort = req.query.Sort;
+    let DestinationList = await modele.find();
+    FilterList.forEach(function (filters) {
+        if (filters.value !== "All") {
+            switch (filters.type) {
+                case "Number":
+                    DestinationList.where(filters.key).equals(filters.value);
+                    break;
 
-        let ArticleList = await modele.find({ "Champ1" : FilterList.Pays, "Champ2": FilterList.ville}, {}).sort({'Article.Date': -1});
-        console.log(ArticleList);
-        res.json(ArticleList);
+                case "Array":
+                    DestinationList.where(filters.value).within(filter.key);
+                    break;
+
+                default:
+                    DestinationList.where(filters.key).equals(filters.value);
+                    break;
+            }
+
+
         }
-      );
-
-        let DestinationList = await modele.find()
-    FilterList.forEach(function(filters){
-if(filters.value !== "All"){
-    switch(filters.type){
-case "Number":
-DestinationList.where(filters.key).equals(filters.value);
-break;
-
-case "Array":
-DestinationList.where(filters.value).within(filter.key);
-break;
-
-default:
-DestinationList.where(filters.key).equals(filters.value);
-break;
-    }
-
- 
-} 
-console.log(DestinationList);
+        console.log(DestinationList);
         res.json(DestinationList);
     });
-
-       
-      
+});
 
 
-    
 
-    app.listen(process.env.PORT ||8014, ()=>{
-        console.log('waf waf 8014 waf waf');
-    })
+
+
+
+app.listen(process.env.PORT || 8014, () => {
+    console.log('waf waf 8014 waf waf');
+})

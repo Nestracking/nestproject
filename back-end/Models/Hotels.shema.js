bed8123372be83d0fecb6eb3044f8b9cac6a9fe3
Kaillensopoulos <arnaud.scieur@gmail.com>
2018-10-19 10:07:37 +0200

    Reservation Model

--- back-end/Models/Hotels.shema.js
+++ back-end/Models/Hotels.shema.js
@@ -1,4 +1,36 @@
 var schema = new Mongoose.Schema({
+        "_id": {
+            "$oid": String,
+        },
+        "SEGMENTATION": String,
+        "COMPANY 1": String,
+        "COMPANY 2": String,
+        "STREET": String,
+        "BUILDING": String,
+        "POSTAL CODE": String,
+        "CITY": String,
+        "COUNTRY": String,
+        "UN GEOSCHEME": String,
+        "CONTINENT": String,
+        "TELEPHONE 1": String,
+        "FAX 1": String,
+        "EMAIL 1": String,
+        "URL 1": String,
+        "ROOMS": Number,
+        "STARS": Number,
+        "BARS": Boolean,
+        "RESTAURANTS": Boolean,
+        "CONFERENCE FACILITIES": Boolean,
+        "GEO LATITUDE": String,
+        "GEO LONGITUDE": String,
+        "Theme": [String],
+        "Views": Number,
+        "Sell": Number,
+        "Price": Number,
+        "Rating": Number,
+        "Number rating": Number,
+        "Comment ID": [{'userid':String,'comment':String}],
+        "Pictures": [String]
 });
 // Schema hotel
 

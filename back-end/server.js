bee7111b713004217b6333da3cf4a0d8fba7e2c8
Chienjaune <manu.lemal@gmail.com>
2018-10-23 10:12:57 +0200

    footer

--- back-end/server.js
+++ back-end/server.js
@@ -26,20 +26,8 @@ app.use(function (req, res, next) {
 });
 
 
-<<<<<<< HEAD
-app.get("/Filter", async function (req, res, ) {
-    let FilterList = req.query.Filter;
-    let Sortitem = req.query.sort;
-    let queryFilter = {};
-    console.log(FilterList);
-    FilterList.forEach(function (filters) {
-        if (filters.value !== "All") {
-            queryFilter[filters.key] = filters.value;
-        }
-=======
 const Filter = require("./Controller/Filter.js")
 app.get("/Filter", Filter);
->>>>>>> e4a294172c6a8f518749e48cfd76e69b80220067
 
 const NewReservation = require("./Controller/NewReservation.js")
 app.post("/NewReservation", NewReservation);

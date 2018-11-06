e65350d72e0e27223dca6992f52e88c432cbb66e
Asteur Florian <asteur.florian@gmail.com>
2018-11-06 16:32:45 +0100

    flo add img accueil DB

--- front-end/nestracking/src/app/index/index.component.ts
+++ front-end/nestracking/src/app/index/index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HTTPRequestService } from '../httprequest.service';
-// import { FilterService } from '../filter.service';
+import { consumeBinding } from '@angular/core/src/render3/instructions';
+
 
 @Component({
   selector: 'app-index',
@@ -9,18 +10,42 @@ import { HTTPRequestService } from '../httprequest.service';
 })
 export class IndexComponent implements OnInit {
 
-  constructor(private httprequest: HTTPRequestService) { }
-
-  ngOnInit() {
-    let themeSun= this.httprequest.Filtering([{key : "Views", value: "All"}],'Views',-1).subscribe((reponse) => {
-      reponse[0]
-      let t = 'URL 1'
-      console.log('reponse',reponse[0]);     
-     
-    })
-    
-    console.log(themeSun);
+ArrayThemes: Array<String>;
+themes: any;
+ArrayMoment: Array<String>;
+moments:any;
+  constructor(private httprequest: HTTPRequestService) { 
+    // this.themes = object dont les clés correspondent à chaque string présente dans this.ArrayThemes 
+    // Et pour chaque clés est attribué l'url de l'image correspondante en provenance de la DataBase
+    this.themes ={}
+    this.ArrayThemes = ['summer','love','last','history']
+    this.moments ={}
+    this.ArrayMoment = ['Views','Sell']
   }
 
+  ngOnInit() {
+    this.ArrayThemes.forEach((themeValue)=>{
+       this.httprequest
+        .Filtering([{key : "Theme", value: themeValue}])
+        .subscribe((reponse) => {            
+          this.themes[`${themeValue}`] = reponse[0].Pictures[0];
+        })
+    })
+    
+    this.ArrayMoment.forEach((Moment)=>{
+      this.httprequest
+      .Filtering([{key : Moment, value: 'All'}],`${Moment}`,-1 )
+      .subscribe((reponse) => {            
+        this.moments[`${Moment}`] = reponse[0].Pictures[0];
+      })
+    })
+  
+      
+  }
+  
+
+  
+  
+  
   
 }

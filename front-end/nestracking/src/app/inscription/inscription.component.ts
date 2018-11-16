f6496584422844ea813794d62bce8cbce0b22fc9
Asteur Florian <asteur.florian@gmail.com>
2018-11-16 12:22:34 +0100

    flo add inscription link

--- front-end/nestracking/src/app/inscription/inscription.component.ts
+++ front-end/nestracking/src/app/inscription/inscription.component.ts
@@ -24,13 +24,11 @@ export class InscriptionComponent implements OnInit {
         Validators.minLength(5),
         Validators.maxLength(25),
         Validators.required,
-        // Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
       ])),
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, Validators.compose([
          Validators.minLength(5),
          Validators.required,
-        //  Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
       ])),
       
       confirm_password: new FormControl(null,Validators.required)
@@ -38,27 +36,18 @@ export class InscriptionComponent implements OnInit {
            
        return PasswordValidator.areEqual(formGroup);
     });
-    
   }
-
 
   onSubmit(objValue) { 
     this.httprequest.UserCreate(objValue).subscribe( response => { 
-      console.log('response: ', response);
-      let userCreated = "Votre compte a bel et bien été créer !";
       let rep: any = response;
-      console.log(rep.Send);
-      console.log(rep.Send.length);
       if(rep.Send.length === 38){
-        console.log('Send Clg');
         alert(rep.Send)
         let accueil :string = '/'
         this.router.navigate([accueil])
       } else {
-        
          alert(rep.Error);
       }
-      
     })
   }
   getErrorMessage(formControlName : string): string {
@@ -71,40 +60,14 @@ export class InscriptionComponent implements OnInit {
       
     }
     if(formControlName === 'confirm_password'){
-      console.log('object', this.loginForm.controls['confirm_password'].valid);
+      // console.log('object', this.loginForm.controls['confirm_password'].valid);
     }
     return Object.keys(this.loginForm.controls[formControlName].errors).reduce(
       (prev, current, currentIndex) => {
-
         return `${errors[current]}`;
       },''
       )
 
-    // if(formControlName === 'confirm_password'){
-      
-    //   let cfPass = this.loginForm.controls[formControlName]
-    //   console.log('PASS:',cfPass.touched);
-    //   console.log('COWBOY: ',cfPass.parent.errors);
-    //   if( cfPass.touched === true){
-    //     console.log('COWBOY: ',this.loginForm.controls[formControlName].parent.errors);
-    //     if(cfPass){
-    //       console.log("object");
-    //       return Object.keys(this.loginForm.controls[formControlName].parent.errors).reduce(
-    //       (prev, current, currentIndex) => {
-    //         console.log('CURRENT',`${errors['email']}`);
-    //        alert('erreur mot de passe')
-    //         return `${errors['email']}`;
-    //       },''
-    //       )
-    //     }
-    //   }
-    // } else{
-    //   return Object.keys(this.loginForm.controls[formControlName].errors).reduce(
-    //   (prev, current, currentIndex) => {
-    //     return `${errors[current]}`;
-    //   },''
-    //   )
-    // }
   }
 }
 
--- front-end/nestracking/src/app/inscription/password.validator.ts
+++ front-end/nestracking/src/app/inscription/password.validator.ts
@@ -2,21 +2,22 @@ import { FormControl, FormGroup, NgForm, FormGroupDirective,AbstractControl } fr
 
 export class PasswordValidator {
   // Inspired on: http://plnkr.co/edit/Zcbg2T3tOxYmhxs7vaAm?p=preview
+  // Récupération valeur des inputs du form d'inscription
+  // Compare password et confirm_password
+  // créer dans formGroup._parent.errors 1 objet {areEqual:true}
   static areEqual(formGroup: FormGroup) {
     let value;
     let valid = true;
-    // console.log('THIS: ',this);
+
     for (let key in formGroup.controls) {
-      console.log("formGroup.controls",formGroup.controls);
+
       if (formGroup.controls.hasOwnProperty(key)) {
-        // console.log('key :', key);
+
         let control: FormControl = <FormControl>formGroup.controls["password"];
         let confirmControl = <FormControl>formGroup.controls["confirm_password"];
-        // console.log('confirmControl',confirmControl.value);
-        // console.log("controls: ",formGroup.controls.hasOwnProperty(key));
 
         if (value === undefined) {
-          // console.log('control.value',control.value);
+
           value = control.value
         } else {
           if (value !== confirmControl.value) {

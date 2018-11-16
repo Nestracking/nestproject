0c3f80f8e695d7096c633bf89bd7b0e7f0afcaab
Kaillensopoulos <arnaud.scieur@gmail.com>
2018-11-16 12:06:55 +0100

    Connection Back End

--- back-end/best-practices-rest-api/api/controllers/user.js
+++ back-end/best-practices-rest-api/api/controllers/user.js
@@ -5,6 +5,9 @@ const express = require('express'),
       ReservationsModel = require("../models/Reservations.schema"),
       UserModel = require("../models/Users");
 
+      'use strict';
+      const fs   = require('fs');
+      const jwt  = require('jsonwebtoken');
       /**
  * This method filter reservations with parameters
  * 
@@ -36,5 +39,43 @@ res("Ce Nom est déja pris !")
 }
 
 exports.connectuser = async function (req, res){
+let ConnectionInfo = req.query.ConnectionInfo;
 
+/// Parsing
+
+let UserFind = await UserModel.find({ 'Name' : UserInfos.Name}, {Password: 1});
+
+if(UserFind[0].Password === ConnectionInfo.Password){
+
+// Private and Public Key
+var privateKEY  = fs.readFileSync('./private.key', 'utf8');
+var publicKEY  = fs.readFileSync('./public.key', 'utf8');
+
+var i  = 'Nestracking';           // Issuer 
+var s  = ConnectionInfo.user;    // Subject 
+var a  = 'http://localhost:4200';     // Audience
+
+// PAYLOAD
+const payload = {
+    User: ConnectionInfo.User,
+    Password: ConnectionInfo.User
+   };
+
+// SIGNING OPTIONS
+const signOptions = {
+    issuer:  i,
+    subject:  s,
+    audience:  a,
+    expiresIn:  "12h",
+    algorithm:  "RS256"
+   };
+
+const JWT = jwt.sign(payload,privateKEY,signOptions);
+console.log(JWT);
+res.json(JWT);
+}else if(UserFind = []){
+res.json({Error: "L'utilisateur n'existe pas"})
+} else {
+res({Error : "Le mot de passe est incorrect, arrêtez d'essayer de vous connectez sur le compte d'un autre !"})    
+}
 }

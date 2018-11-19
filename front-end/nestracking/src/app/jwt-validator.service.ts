fec080f2856278974f8030e9695cad2b62804889
Asteur Florian <asteur.florian@gmail.com>
2018-11-19 10:15:45 +0100

    flo add jwtValidator

--- front-end/nestracking/src/app/jwt-validator.service.ts
+++ front-end/nestracking/src/app/jwt-validator.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtValidatorService {
 
-  constructor() { }
+  ServerAdress = "http://localhost:8014";
+  loggedIn : boolean = false;
+  admin : boolean = false;
+
+  constructor(private http: HttpClient) {
+   
+  }
+  isLoggedIn():boolean{
+    return this.loggedIn;
+  }
+  isAdmin():boolean{ 
+    return this.admin;
+  }
+  authenticate(userJwt):boolean{
+    let params = new HttpParams();
+    params = params.set('user', userJwt);
+    
+    let authUser = this.http.get(this.ServerAdress + '/user/',{ params: params } )
+    
+    if(authUser){
+      this.loggedIn = true;
+      // if(authUser.role === 'admin'){
+      //   this.admin = true; 
+      // }
+      // return true;
+    } else{
+      return false
+    }
+  }
+  logout() { 
+    this.loggedIn = false;
+  }
 }
